81d187079be700a641afffc1158e4e153651d155
algernon-A <aeneas@aloku.net>
2022-09-11 23:25:08 +1000

    Update commons.

--- Code/Loading.cs
+++ Code/Loading.cs
@@ -5,24 +5,12 @@
 
 namespace TransitVehicleSpawnDelay
 {
-    using AlgernonCommons.UI;
-    using ICities;
+    using AlgernonCommons;
 
     /// <summary>
     /// Main loading class: the mod runs from here.
     /// </summary>
-    public class Loading : LoadingExtensionBase
+    public class Loading : LoadingBase<OptionsPanel>
     {
-        /// <summary>
-        /// Called by the game when level loading is complete.
-        /// </summary>
-        /// <param name="mode">Loading mode (e.g. game, editor, scenario, etc.)</param>
-        public override void OnLevelLoaded(LoadMode mode)
-        {
-            base.OnLevelLoaded(mode);
-
-            // Set up options panel event handler (need to redo this now that options panel has been reset after loading into game).
-            OptionsPanelManager<OptionsPanel>.OptionsEventHook();
-        }
     }
 }
--- Code/Mod.cs
+++ Code/Mod.cs
@@ -5,24 +5,19 @@
 
 namespace TransitVehicleSpawnDelay
 {
-    using AlgernonCommons;
     using AlgernonCommons.Patching;
     using AlgernonCommons.Translation;
-    using AlgernonCommons.UI;
-    using ColossalFramework.UI;
     using ICities;
 
     /// <summary>
     /// The base mod class for instantiation by the game.
     /// </summary>
-    public class Mod : PatcherMod, IUserMod
+    public class Mod : PatcherMod<OptionsPanel, PatcherBase>, IUserMod
     {
-        private static readonly string ModName = "Transit Vehicle Spawn Delay";
-
         /// <summary>
-        /// Gets the mod's name for logging purposes.
+        /// Gets the mod's base display name (name only).
         /// </summary>
-        public override string LogName => ModName;
+        public override string BaseName => "Transit Vehicle Spawn Delay";
 
         /// <summary>
         /// Gets the mod's unique Harmony identfier.
@@ -30,45 +25,9 @@ namespace TransitVehicleSpawnDelay
         public override string HarmonyID => "com.github.algernon-A.csl.tvsd";
 
         /// <summary>
-        /// Gets the mod's display name.
-        /// </summary>
-        public override string Name => ModName + ' ' + AssemblyUtils.TrimmedCurrentVersion;
-
-        /// <summary>
         /// Gets the mod's description for display in the content manager.
         /// </summary>
         public string Description => Translations.Translate("VSD_DESC");
-
-        /// <summary>
-        /// Called by the game when the mod is enabled.
-        /// </summary>
-        public override void OnEnabled()
-        {
-            base.OnEnabled();
-
-            // Add the options panel event handler for the start screen (to enable/disable options panel based on visibility).
-            // First, check to see if UIView is ready.
-            if (UIView.GetAView() != null)
-            {
-                // It's ready - attach the hook now.
-                OptionsPanelManager<OptionsPanel>.OptionsEventHook();
-            }
-            else
-            {
-                // Otherwise, queue the hook for when the intro's finished loading.
-                LoadingManager.instance.m_introLoaded += OptionsPanelManager<OptionsPanel>.OptionsEventHook;
-            }
-        }
-
-        /// <summary>
-        /// Called by the game when the mod options panel is setup.
-        /// </summary>
-        /// <param name="helper">UI helper instance.</param>
-        public void OnSettingsUI(UIHelperBase helper)
-        {
-            // Create options panel.
-            OptionsPanelManager<OptionsPanel>.Setup(helper);
-        }
 
         /// <summary>
         /// Saves settings file.
